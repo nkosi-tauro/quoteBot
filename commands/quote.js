455952aa5b3a8257c44fee8a4e73bd6531c09150
Yaseen-Davids <yaseendavids477@gmail.com>
2019-11-01 22:02:11 +0200

    added extras

--- commands/quote.js
+++ commands/quote.js
@@ -1,15 +1,10 @@
- 
+
 module.exports = message => {
-    let text="";//intial set
- //for loop which runs three times
-   fetch("https://api.quotable.io/random") //fetch api for 3 random chuck norris jokes
-     .then(res => res.json()) //read the json
-     .then((data) => { 
-           
-        let text = `${data.content} —${data.author}`; 
-        message.reply(text)
-        console.log(text)
-        
-         })
- 
+  let text = "";
+  fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then((data) => {
+      text = `${data.content} —${data.author}`;
+      message.reply(text);
+    })
 };
